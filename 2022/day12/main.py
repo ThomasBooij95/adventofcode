f1983f404a61c50d2dee9e6a6681925525df07c9
Thomas Booij <thomasbooij95@gmail.com>
2023-01-03 14:29:45 +0100

    First try day 12

--- 2022/day12/main.py
+++ 2022/day12/main.py
@@ -1,5 +1,6 @@
 from import_text_file import import_text_file
 import numpy as np
+import logging
 
 
 def parse_to_grid(input: str):
@@ -23,21 +24,44 @@ def traverse(grid):
     cur_height = to_value(grid[cur_index[0], cur_index[1]])
     width_grid = len(grid[0])
     heigth_grid = len(grid)
-    print(cur_height)
-    # print(DIRECTIONS)
+    last_index = get_starting_index(grid)
+    steps = 0
+    while (get_value_of_index(grid, cur_index) < 27):
+        cur_index, last_index = get_next_index(
+            grid, cur_index, width_grid, heigth_grid, last_index)
+        steps += 1
+
+    print(get_value_of_index(grid, cur_index))
+    print('new node = ', cur_index)
+    print('nr of steps = ', steps)
+    return steps
+
+
+def get_next_index(grid, cur_index, width_grid, heigth_grid, last_index):
+    cur_height = get_value_of_index(grid, cur_index)
     for _, dir in DIRECTIONS.items():
         checking_index = sum_lists(cur_index, dir)
-        if checking_index[0] < 0 or checking_index[0] > heigth_grid or checking_index[1] < 0 or checking_index[1] > width_grid:
-            print('Out of Bounds!')
+        if checking_index[0] < 0 or checking_index[0] >= heigth_grid or checking_index[1] < 0 or checking_index[1] >= width_grid:
+            logging.debug('Out of Bounds!')
             continue
+        if checking_index == last_index:
+            logging.debug('Back is not the way!')
+            continue
+        cur_neighbour_height = get_value_of_index(grid, checking_index)
+        logging.debug(cur_neighbour_height)
+        if cur_neighbour_height <= cur_height+1:
+            last_index = cur_index
+            cur_index = checking_index
+            break
+    return cur_index, last_index
 
-        print(checking_index)
+
+def get_value_of_index(grid, checking_index):
+    return to_value(grid[checking_index[0], checking_index[1]])
 
 
 def sum_lists(cur_index, dir):
     return [sum(x) for x in zip(cur_index, dir)]
-    # print(grid[start_index[0], start_index[1]])
-    # print(to_value(grid[0, 0]))
 
 
 def to_value(input):
@@ -48,13 +72,16 @@ def to_value(input):
     return ord(input)-96
 
 
-def get_starting_index(grid) -> list[int]:
+def get_starting_index(grid) -> 'list[int]':
     start_index = list(list(zip(*np.where(grid == "S")))[0])
     return start_index
 
 
 if __name__ == '__main__':
-    parsed_input = import_text_file(isDemoSet=True)
+    # logging.basicConfig(level=logging.DEBUG)
+    parsed_input = import_text_file(isDemoSet=False)
     grid = parse_to_grid(parsed_input)
     print(grid)
-    traverse(grid)
+    answer_part1 = traverse(grid)
+    print('Answer to part 1 is:', answer_part1)
+    # print('Answer to part 2 is:')
